97f7984c99db0c7b98209854dcae25674b3b8973
Mani Shidfar <mreza.shidfar@gmail.com>
2023-04-06 15:41:59 +0200

    done

--- main.js
+++ main.js
@@ -1,20 +1,26 @@
 function getDogs() {
-  // this is the skeleton for the fetch function
-  // fetch all the names of breeds so that breedNames is not an empty array
-  // you may have to use Object.keys() method...
-  // each breed should then be the title of the card and the alt of the image (which will come in the following task)
-  // the code for the card has already been written for you
+  fetch('https://dog.ceo/api/breeds/list/all')
+    .then(response => response.json())
+    .then(data => {
+      const breeds = data.message;
+      const breedNames = Object.keys(breeds);
 
-  const breedNames = []
-
-  const container = document.querySelector('.container-grid');
-
-  container.innerHTML = breedNames.map(breed => `
-     <div class="card-body">
-     <img class="card-img-top" src="" alt="" style="object-fit: cover; object-position: center;" height="300px" />
-     <h5 class="card-title">Breed name</h5>
-     </div>
-     </div>`).join('');
+      breedNames.forEach(breed => {
+        fetch(`https://dog.ceo/api/breed/${breed}/images/random`)
+          .then(response => response.json())
+          .then(data => {
+            const img = data.message
+            const container = document.querySelector('.container-grid');
+            container.innerHTML += `<div class="card">
+              <img class="card-img-top" src="${img}"  height="300px" style="object-fit: cover; object-position: center;">
+               <div class="card-body">
+               <h5 class="card-title">${breed}</h5>
+               </div>
+            </div>`;
+          });
+      });
+    });
 }
+
 
 getDogs();
